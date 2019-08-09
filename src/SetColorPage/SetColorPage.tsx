984f31cfb2843bee128ad1f3c447bb39c458b76e
Sergey Stadnik <sergey.stadnik@marketplacer.com>
2019-08-09 13:41:13 +1000

    Send HTTP POST request to change color

--- src/SetColorPage/SetColorPage.tsx
+++ src/SetColorPage/SetColorPage.tsx
@@ -1,24 +1,30 @@
 import React, { useState } from 'react'
 import { Button } from 'antd'
 import { CirclePicker } from 'react-color'
-import axios from 'axios'
+
+// IP: 192.168.180.18
 
 const setLedColor = (color: string) => {
-  const colorInt = parseInt(color, 16)
-  axios
-    .post('http://indicator.local/setLeds', {
+  const colorInt = parseInt(color.slice(1), 16) // Strip leading '#', convert to int
+  fetch('http://indicator.local/setled', {
+    method: 'POST',
+    mode: 'no-cors',
+    body: JSON.stringify({
       allRgb: colorInt
     })
-    .catch(err => {
-      console.log('Error setting color:', err)
-    })
+  }).catch(err => {
+    console.log('Error setting color:', err)
+  })
 }
 
 const SetColorPage: React.FC<{}> = () => {
   const [color, setColor] = useState('0') // Color as RGB hex string
   return (
     <div>
-      <CirclePicker onChangeComplete={color => setColor(color.hex)} />
+      <CirclePicker
+        color={color}
+        onChangeComplete={color => setColor(color.hex)}
+      />
       <Button type="primary" onClick={() => setLedColor(color)}>
         Submit
       </Button>
